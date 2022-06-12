a288fc54f6cc3ff12ffac94b3e91c93b92b36f68
braifz <braian94lp@gmail.com>
2022-06-12 14:53:23 -0300

    App component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Brain from "./components/Brain/Brain";
+import Header from "./components/Header";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Header title="title" text="some texts" /> */}
+      <Brain />
+    </div>
+  );
+};
+
+export default App;
