4b6f4bd6c4ae9c6e0b43a41ada7ace4990dc323f
braifz <braian94lp@gmail.com>
2022-06-15 10:30:30 -0300

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Brain from "./components/Brain/Brain";
 import Header from "./components/Header";
+import Cube from "./components/Cube";
 import "./App.css";
+import ThreeScene from "./components/ThreeScene";
+import { OrbitControls } from "@react-three/drei";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Header title="title" text="some texts" /> */}
-      <Brain />
+      <Header title="title" text="some texts" />
+      <ThreeScene>
+        <Cube />
+        <OrbitControls />
+        <ambientLight />
+      </ThreeScene>
     </div>
   );
 };
